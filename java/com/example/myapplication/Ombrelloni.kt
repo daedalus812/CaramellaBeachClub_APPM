f2d229d02c10f9813e09276754ffda8049b0044f
Domefemia <domefemia@hotmail.it>
2023-12-14 17:31:48 +0100

    Cambiate icona disconneti, creata logica salvataggio ombrelloni

--- java/com/example/myapplication/Ombrelloni.kt
+++ java/com/example/myapplication/Ombrelloni.kt
@@ -1,19 +1,19 @@
 package com.example.myapplication
 
-import android.content.Intent
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.ImageButton
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class Ombrelloni : AppCompatActivity() {
     private val numOmbrelli = 9
     private val booleanArray = BooleanArray(numOmbrelli)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_umbrella)
+        load()
     }
 
     fun processUmbrella(view: View) {
@@ -31,5 +31,44 @@ class Ombrelloni : AppCompatActivity() {
         }
     }
 
+    fun indietro(view: View) {
+        save()
+        finish()
+    }
+
+    fun svuota(view: View){
+        val prefs = getSharedPreferences("data", Context.MODE_PRIVATE)
+        val editor = prefs.edit()
+        for (i in 0 until numOmbrelli) {
+            booleanArray[i] = false
+            editor.putBoolean(i.toString(), false)
+            val id = resources.getIdentifier("umbrella"+(i+1)+"Button", "id", packageName)
+            val bottone = findViewById<Button>(id)
+            bottone.text = "Occupa"
+        }
+        editor.apply()
+    }
+
+    private fun save() {
+        val prefs = getSharedPreferences("data", Context.MODE_PRIVATE)
+        val editor = prefs.edit()
+        for (i in 0 until numOmbrelli)
+            editor.putBoolean(i.toString(), booleanArray[i])
+        editor.apply()
+
+    }
+
+    private fun load(){
+        val prefs = getSharedPreferences("data", Context.MODE_PRIVATE)
+        for (i in 0 until numOmbrelli) {
+            booleanArray[i] = prefs.getBoolean(i.toString(), false)
+            val id = resources.getIdentifier("umbrella"+(i+1)+"Button", "id", packageName)
+            val bottone = findViewById<Button>(id)
+            if (booleanArray[i])
+                bottone.text = "Libera"
+            else
+                bottone.text = "Occupa"
+        }
+    }
 
 }
