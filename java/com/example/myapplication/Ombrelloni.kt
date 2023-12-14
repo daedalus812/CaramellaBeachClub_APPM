b16b7e98764271fc3ec0653d2e935085b347d551
Lorenzo <lorenzofanelli02@icloud.com>
2023-12-14 22:57:30 +0100

    Rivisitato function "processUmbrella", da aggiungere l'alert dialog

--- java/com/example/myapplication/Ombrelloni.kt
+++ java/com/example/myapplication/Ombrelloni.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class Ombrelloni : AppCompatActivity() {
@@ -23,8 +25,15 @@ class Ombrelloni : AppCompatActivity() {
             val buttonIdName = resources.getResourceEntryName(buttonId)
             val i = Character.getNumericValue(buttonIdName[8].code) - 1
             booleanArray[i] = !booleanArray[i]
-            if (booleanArray[i])
+            if (booleanArray[i]) {
                 umbrella.text = "Libera"
+
+                //alertDialog
+
+                val idText = resources.getIdentifier("TextView" + i, "id", packageName)
+                val textView = findViewById<Button>(idText)
+                textView.visibility = View.VISIBLE
+            }
             else
                 umbrella.text = "Occupa"
 
@@ -36,13 +45,13 @@ class Ombrelloni : AppCompatActivity() {
         finish()
     }
 
-    fun svuota(view: View){
+    fun svuota(view: View) {
         val prefs = getSharedPreferences("data", Context.MODE_PRIVATE)
         val editor = prefs.edit()
         for (i in 0 until numOmbrelli) {
             booleanArray[i] = false
             editor.putBoolean(i.toString(), false)
-            val id = resources.getIdentifier("umbrella"+(i+1)+"Button", "id", packageName)
+            val id = resources.getIdentifier("umbrella" + (i + 1) + "Button", "id", packageName)
             val bottone = findViewById<Button>(id)
             bottone.text = "Occupa"
         }
@@ -58,11 +67,11 @@ class Ombrelloni : AppCompatActivity() {
 
     }
 
-    private fun load(){
+    private fun load() {
         val prefs = getSharedPreferences("data", Context.MODE_PRIVATE)
         for (i in 0 until numOmbrelli) {
             booleanArray[i] = prefs.getBoolean(i.toString(), false)
-            val id = resources.getIdentifier("umbrella"+(i+1)+"Button", "id", packageName)
+            val id = resources.getIdentifier("umbrella" + (i + 1) + "Button", "id", packageName)
             val bottone = findViewById<Button>(id)
             if (booleanArray[i])
                 bottone.text = "Libera"
@@ -70,5 +79,4 @@ class Ombrelloni : AppCompatActivity() {
                 bottone.text = "Occupa"
         }
     }
-
 }
