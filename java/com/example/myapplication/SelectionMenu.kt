4f902d666e244a1db48b2649f5afeffcd8987604
Lorenzo <lorenzofanelli02@icloud.com>
2023-12-19 12:35:33 +0100

    - Menu (ver 2.1) implemented (minor fixes) - Added background image on Selection Menu - Color icon changed to match caramella style - Rounded buttons added

--- java/com/example/myapplication/SelectionMenu.kt
+++ java/com/example/myapplication/SelectionMenu.kt
@@ -1,19 +1,31 @@
 package com.example.myapplication
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.VideoView
+
 
 class SelectionMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_selection_menu)
+        setContentView(R.layout.activity_selection_menu) // Muovi questa chiamata qui
+
+        // Assicurati di usare l'ID corretto
+        val backgroundImageView: ImageView = findViewById(R.id.backgroundImageView)
+
+        // Verifica se l'oggetto non è nullo prima di chiamare il metodo setImageResource
+
+        backgroundImageView.setImageResource(R.drawable.background_image)
+
+
         val menu = findViewById<ImageButton>(R.id.menuButton)
         val umbrella = findViewById<ImageButton>(R.id.umbrellaButton)
         val shop = findViewById<ImageButton>(R.id.shopButton)
         val back = findViewById<ImageButton>(R.id.backButton)
-
 
         menu.setOnClickListener {
             val intent = Intent(this, Menu::class.java)
@@ -31,7 +43,5 @@ class SelectionMenu : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
-
-
     }
 }
